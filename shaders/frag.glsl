d9db52073daa7f23a348c8e0ba5dd808f514d8d5
AcChosen <acchosen2@gmail.com>
2025-03-12 14:26:19 -0400

    Slide Update 2

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -21,13 +21,13 @@ void main() {
 
     float stripeCount = 16.0;
     // float animatedCircleY = uv.y - iTime * 0.05;
-    float scroll = iTime * 0.5;
+    float scroll = iTime * 1.0;
     float animatedBgY = uv.y;
 
     // float circleStripeValue = fract(animatedCircleY * stripeCount) < 0.5 ? 0.0 : 1.0;
     float bgStripeValue = fract(animatedBgY * stripeCount) < 0.5 ? 0.0 : 1.0;
 
-    float blueGradient = clamp(sin((uv.y+scroll) * 3.0), 0.0, 1.0) ;
+    float blueGradient = clamp(sin((uv.y+scroll) * 2.0), 0.0, 1.0) ;
 
     // vec3 circleBlueColor = vec3(0.0, 0.5, 0.8) * (blueGradient * 0.7 + 0.3);
     // vec3 circleStripeColor = mix(vec3(0.0), circleBlueColor, circleStripeValue);
